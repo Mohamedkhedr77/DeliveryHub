8c50959083d0eb367ad1921ecd44033d1e13b6b4
Mohamedkhedr77 <124399025+Mohamedkhedr77@users.noreply.github.com>
2026-06-24 14:57:31 +0300

    let's start

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <x-slot:title>
+        {{ $title }}
+    </x-slot:title>
+
+    <div class="py-10 text-center">
+        <h1 class="text-4xl font-bold text-success">أهلاً بك في DeliveryHub!</h1>
+        <p class="mt-4 text-lg text-base-content/70">مرحباً بك في تطبيق DeliveryHub! نحن سعداء بوجودك معنا.</p>
+        <button class="btn btn-primary mt-6">start </button>
+    </div>
+</x-layout>
